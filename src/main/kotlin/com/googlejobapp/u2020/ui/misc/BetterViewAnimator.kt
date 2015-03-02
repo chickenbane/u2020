cf3f237dfb62e06f95d67a3e37d40260d05d3fe9
Joey T <thechickenbane@gmail.com>
2015-03-01 21:38:13 -0800

    Class 4: BetterViewAnimator

---
+++ src/main/kotlin/com/googlejobapp/u2020/ui/misc/BetterViewAnimator.kt
@@ -0,0 +1,26 @@
+package com.googlejobapp.u2020.ui.misc
+
+import android.widget.ViewAnimator
+import android.util.AttributeSet
+import android.content.Context
+
+/**
+ * Created by joeyt on 3/1/15.
+ */
+class BetterViewAnimator(context: Context, attrs: AttributeSet) : ViewAnimator(context, attrs) {
+    var displayedChildId: Int
+        get() = getChildAt(getDisplayedChild()).getId()
+        set(id) {
+            if (displayedChildId == id) {
+                return
+            }
+            val count = getChildCount()
+            for (i in 0..count-1) {
+                if (getChildAt(i).getId() == id) {
+                    setDisplayedChild(i)
+                    return
+                }
+            }
+            throw IllegalArgumentException("No view with ID $id")
+        }
+}
